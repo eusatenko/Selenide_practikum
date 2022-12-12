8918b32f1986626ea65e2462341b93728119d55d
Evgeny Usatenko <usatenko_e@mail.ru>
2022-12-12 15:59:53 +0300

    в github

---
+++ src/test/java/AutoTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AutoTest {
+
+    @Test
+    public void authTest() {
+
+        // открывается страница и создаётся экземпляр класса страницы
+        LoginPageMestoSelenide loginPage =
+                open("https://qa-mesto.praktikum-services.ru/",
+                        LoginPageMestoSelenide.class);
+
+        // вход в приложение
+        loginPage.login("nacalo3463@kixotic.com",
+                "Qwerty123");
+
+        $(byClassName("header__user")).shouldHave(exactText("acalo3463@kixotic.com"));
+
+    }
+}
